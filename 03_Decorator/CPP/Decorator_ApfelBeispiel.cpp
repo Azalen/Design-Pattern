230e5400b299364ee7be8d4f0a1fe8cfa42baede
Azalen <bastiari@yahoo.de>
2022-03-24 16:04:15 +0100

    Create Decorator_ApfelBeispiel.cpp

---
+++ 03_Decorator/CPP/Decorator_ApfelBeispiel.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+// Virtuelle Basisklasse == Interface
+// <<interface>> Komponente
+class Apfel {
+    public:
+        Apfel(){}
+        virtual ~Apfel(){}
+        virtual void ausgabe() = 0;
+        double gewicht;
+};
+
+// Konkrete Komponente
+class KonkreterApfel : public Apfel {
+    public:
+        KonkreterApfel(double gewicht) : Apfel() {
+            this->gewicht = gewicht;
+        }
+        ~KonkreterApfel(){ cout << "KonkreterApfel destructed" << endl; }
+
+        void ausgabe(){ cout << gewicht << endl; } 
+};
+
+// Dekorierer
+class Decorator : public Apfel {
+    protected:
+        Apfel * component;
+
+        Decorator(double gewicht) : Apfel() {
+            this->component = nullptr;
+            this->gewicht = gewicht;
+        }
+        virtual ~Decorator() {
+            delete component;
+        }
+
+        virtual void ausgabe() {
+            component->ausgabe();
+        }
+};
+
+// Konkreter Dekorierer A
+class FarbigerApfel : public Decorator {
+    public:
+        FarbigerApfel(Apfel * component, const string & farbe) : Decorator(component->gewicht) {
+            Decorator::component = component;
+            this->farbe = farbe;
+        }
+
+        void ausgabe() {
+            Decorator::ausgabe();
+            cout << farbe << endl;
+        }
+    private:
+        string farbe;
+};
+
+int main() {
+
+    system("clear");
+    cout << endl;
+
+    Apfel * a = new FarbigerApfel(new KonkreterApfel(10), "blau");
+    a->ausgabe();
+
+    return 0;
+}
